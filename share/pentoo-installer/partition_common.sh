414ece9c668d7da0461584a33150d69955996ec2
Wuodan0 <Wuodan0@gmail.com>
2014-11-18 21:34:48 +0000

    Changed partition/disk selection. Disabled selection of extended-partition

--- share/pentoo-installer/partition_common.sh
+++ share/pentoo-installer/partition_common.sh
@@ -20,28 +20,6 @@ source "${SHAREDIR}"/common.sh || exit $?
 ##############################
 ## START: utility functions ##
 
-# partition_getavaildisks()
-# Get a list of available disks for use in the "Available disks" dialogs. This
-# will print the disks as follows, getting size info from partition_getdisccapacity():
-#   /dev/sda: 625000 MiB (610 GiB)
-#   /dev/sdb: 476940 MiB (465 GiB)
-#
-# parameters: none
-#
-partition_getavaildisks() {
-	# check input
-	check_num_args "${FUNCNAME}" 0 $# || return $?
-	local _DISC=""
-	local _DISC_LIST=""
-	local _DISC_SIZE=""
-	_DISC_LIST=$(partition_finddisks) || return $?
-    for _DISC in ${_DISC_LIST}; do
-        _DISC_SIZE=$(partition_getdisccapacity $_DISC) || return $?
-        echo "${_DISC}: $((_DISC_SIZE / 2**20)) MiB ($((_DISC_SIZE / 2**30)) GiB)\n" || return $?
-    done
-	return 0
-}
-
 # partition_finddisks()
 # prints devices to STDOUT
 #
@@ -108,10 +86,11 @@ partition_findpartitions() {
         _DISC=$(echo ${_DEVPATH} | sed 's|.*/||') || return $?
         for _PARTPATH in "/sys/block/${_DISC}/${_DISC}"*; do
 			_PART="$(basename ${_PARTPATH})" || return $?
+			fdisk -l /dev/"${_DISC}" | grep "^/dev/${_PART}[[:space:]]" | awk '{print $5}' 1>&2
             # check if not already assembled to a raid device
             if ! cat /proc/mdstat 2>/dev/null | grep -q "${_PART}" \
 				&& ! file -s /dev/"${_PART}" | grep -qi lvm2 \
-				&& ! fdisk -l /dev/"${_DISC}" | grep "^/dev/${_PART}[[:space:]]" | awk '{print $6}' | grep -q 5 \
+				&& ! fdisk -l /dev/"${_DISC}" | grep "^/dev/${_PART}[[:space:]]" | awk '{print $5}' | grep -q 5 \
 				; then
 				if [ -d "${_PARTPATH}" ]; then
                     echo "/dev/${_PART}"
@@ -152,27 +131,47 @@ partition_findpartitions() {
 partition_getdisccapacity() {
 	# check input
 	check_num_args "${FUNCNAME}" 1 $# || return $?
-    fdisk -l $1 | sed -n '2p' | cut -d' ' -f5 || return $?
+    fdisk -l "${1}" | sed -n '2p' | cut -d' ' -f5 || return $?
+	return 0
+}
+
+# partition_getdisccapacity_formatted()
+# outputs formatted disc capacity
+# Example:
+#  625000 MiB (610 GiB)
+#
+# parameters: device file
+#
+partition_getdisccapacity_formatted() {
+	# check input
+	check_num_args "${FUNCNAME}" 1 $# || return $?
+	local _DISC_SIZE=""
+	_DISC_SIZE=$(partition_getdisccapacity "${1}") || return $?
+	_DISC_SIZE="$((_DISC_SIZE / 2**20)) MiB ($((_DISC_SIZE / 2**30)) GiB)" || return $?
+	echo -n "${_DISC_SIZE}"
 	return 0
 }
 
 # partition_selectdisk()
-# displays available disks so user can select one
+# displays disks so user can select one
 # prints disk to STDOUT
 #
 # exits: !=1 is an error
 #
 partition_selectdisk() {
-    local _DISC=""
-    local _DISCS=""
-    local _DISCS_AVAILABLE=""
-	_DISCS_AVAILABLE=$(partition_getavaildisks) || return $?
-	_DISCS=$(partition_finddisks) || return $?
-	_DISCS=$(add_option_label "${_DISCS}" '-') || return $?
-    _DISCS="${_DISCS} OTHER -"
-	show_dialog --msgbox "Available Disks:\n\n${_DISCS_AVAILABLE}\n" 0 0
+    local _DISC=
+    local _DISCS=
+	local _MENU_ITEMS=()
+	_DISCS="$(partition_finddisks)" || return $?
+	for _DISC in ${_DISCS}; do
+		# add disc and description to array
+		_MENU_ITEMS+=("${_DISC}" "$(partition_getdisccapacity_formatted "${_DISC}")") \
+			|| return $?
+	done
+	_MENU_ITEMS+=('OTHER' '-')
+	# expand array below
 	_DISC="$(show_dialog --menu "Select the disk you want to use" \
-			0 0 7 ${_DISCS})" \
+			0 0 7 "${_MENU_ITEMS[@]}")" \
 			|| return $?
 	if [ "${_DISC}" = "OTHER" ]; then
 		_DISC="$(show_dialog --inputbox "Enter the full path to the device you wish to use" \
@@ -184,33 +183,36 @@ partition_selectdisk() {
 			return "${ERROR_CANCEL}"
 		fi
 	fi
-	# sanity check
-	# TODO remove for public use
-	# if [ "${_DISC}" = "/dev/sda" ] || [ "${_DISC}" = "/dev/sdb" ]; then
-		# echo "EXITING FOR SAFETY, NOT TESTING ON /dev/sd[ab]!" 1>&2
-		# exit 1
-	# fi
-	# everything ok,_DISC_LIST print result to STDOUT
+	# everything ok, print result to STDOUT
 	echo "${_DISC}"
 	return 0
 }
 
 # partition_selectpartition()
-# displays available partitions so user can select one
+# displays partitions so user can select one
 # prints partition to STDOUT
 #
 # exits: !=1 is an error
 #
 partition_selectpartition() {
-    local _PART=""
-    local _PARTS=""
-    local _DISCS_AVAILABLE=""
-	_DISCS_AVAILABLE=$(partition_getavaildisks) || return $?
+    local _PART=
+    local _PARTS=
+    local _DISC=
+    local _DISCS=
+	local _MSG=
+	_DISCS="$(partition_finddisks)" || return $?
+	# compose dialog message
+	_MSG="Available Disks:\n"
+	for _DISC in ${_DISCS}; do
+		# add disc and description to message
+		_MSG="${_MSG}\n${_DISC} $(partition_getdisccapacity_formatted "${_DISC}")" \
+			|| return $?
+	done
+	_MSG="${_MSG}\n\nSelect the partition you want to use"
 	_PARTS="$(partition_findpartitions)" || return $?
 	_PARTS="$(add_option_label "${_PARTS}" '-')" || return $?
     _PARTS="${_PARTS} OTHER -"
-	show_dialog --msgbox "Available Disks:\n\n${_DISCS_AVAILABLE}\n" 0 0
-	_PART="$(show_dialog --menu "Select the partition you want to use" \
+	_PART="$(show_dialog --menu "${_MSG}" \
 			0 0 7 ${_PARTS})" \
 			|| return $?
 	if [ "${_PART}" = "OTHER" ]; then
