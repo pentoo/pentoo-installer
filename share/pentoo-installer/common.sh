c9ee54409b1b367391c0bd8a83422a1a6f15a026
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2018-03-16 17:14:34 -0400

    add more fs types for fun

--- share/pentoo-installer/common.sh
+++ share/pentoo-installer/common.sh
@@ -160,6 +160,8 @@ get_supportedFS() {
 	[ "$(which mkfs.jfs 2>/dev/null)" ]		&& echo -n ' jfs'
 	[ "$(which mkfs.vfat 2>/dev/null)" ]	&& echo -n ' vfat'
 	[ "$(which mkfs.fat 2>/dev/null)" ]	&& echo -n ' fat32'
+	[ "$(which mkfs.f2fs 2>/dev/null)" ] && echo -n ' f2fs'
+	[ "$(which mkfs.reiserfs 2>/dev/null)" ] && echo -n ' reiserfs'
 	return 0
 }
 
